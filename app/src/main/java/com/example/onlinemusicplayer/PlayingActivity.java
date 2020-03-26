1efc06e351f4439ab038c2061313d537321d479a
rastogiakshay <rastogiakshay@live.com>
2020-03-26 10:24:54 +0530

    #Added Now Playing Layout

---
+++ app/src/main/java/com/example/onlinemusicplayer/PlayingActivity.java
@@ -0,0 +1,12 @@
+package com.example.onlinemusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class PlayingActivity extends AppCompatActivity {
+
+    private ArrayList<TrackInfo> trackInfo = new ArrayList<TrackInfo>();
+
+
+}
---
+++ app/src/main/java/com/example/onlinemusicplayer/TrackInfo.java
@@ -0,0 +1,14 @@
+package com.example.onlinemusicplayer;
+
+public class TrackInfo {
+    public String trackName, trackArtist, trackAlbum;
+
+    public TrackInfo() {
+    }
+
+    public TrackInfo(String trackName, String trackArtist, String trackAlbum) {
+        this.trackName = trackName;
+        this.trackArtist = trackArtist;
+        this.trackAlbum = trackAlbum;
+    }
+}
